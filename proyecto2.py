fb6339803d7efab8aee4ea2c3f5a572e8142dc94
yeisonalex33 <yeisonalex.33@hotmail.com>
2023-10-22 18:55:12 -0500

    proyecto_integrador_parte_2

---
+++ proyecto2.py
@@ -0,0 +1,11 @@
+from readchar import readkey, key, readchar
+
+
+while True:
+        print ("ingresa un tecla: ")
+        caracter = readkey()
+        if caracter == key. UP:
+            print(f"has precionado la tecla ↑ UP!")
+            break
+        else:
+            print(f"has presionado la tecla {caracter}, sigue intentando")
