2b5e6b6ef7f898192c2dce075a94a0720b4c0ae8
yeisonalex33 <yeisonalex.33@hotmail.com>
2023-10-25 22:35:02 -0500

    proyecto_3

---
+++ proyecto3.py
@@ -0,0 +1,29 @@
+import os
+from readchar import readkey
+
+#definimos la funcion para limpiar la terminal
+def clean():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+
+#definimos nuestra variable de tipo entero que almacenará el numero que va aumentando
+number = 0
+
+print("Presione la tecla 'n' para iniciar el conteo")
+
+#realizamos el bucle
+while True:
+    key = readkey()
+
+    if key == 'n':
+        number += 1
+        clean()
+        print(f"Has presionado la tecla '{key}' {number} veces de 60 posibles, sigue así")
+
+        if number == 60:
+            clean()
+            print(f"Felicitaciones, has presionado exitosamente la tecla 'n' {number} veces")
+            break
+
+    else:
+        print(f"Has presionado la tecla {key} en lugar de 'n', intentalo de nuevo")        
